9d68d932e9d4b8fdb0be3613563bd1b506ebf40c
Valkyrux <philadephos.granata@gmail.com>
2021-11-29 16:09:35 +0100

    commenti

--- jsnack-palindromo/js/script.js
+++ jsnack-palindromo/js/script.js
@@ -1,24 +1,31 @@
 // funzione che fa il controllo della parola costruendo la parola rovesciata, resituisce un true o false come valore
 function getPalindrome (word) {
-    let controlWord = word.toLowerCase();
-    
-    let reverseWord = "";
+    // variabile di ritorno
     let matching = false;
-
+    // variabili di controllo
+    let controlWord ="";
+    let reverseWord = "";
+    // ciclo che toglie gli spazi
+    for(let i = 0; i < word.length ; i++) {
+        if (word[i] != " ") {
+            controlWord += word[i];
+        }
+    }
+    // tutto in lowercase
+    controlWord = controlWord.toLowerCase();
+    // costruisco la parola contraria
     for(let i = controlWord.length - 1; i >= 0 ; i--) {
         reverseWord += controlWord[i];
     }
-    
+    // controllo se la parola matcha con quella contraria
     if (reverseWord == controlWord) {
         matching = true;
     } 
-
     return matching;
 }
-
-
+// input utente
 let inputWord =  prompt("Inserisci una parola");
-
+// risposta della macchina
 if(getPalindrome(inputWord)) {
     console.log("é un palindromo!");
 } else {
--- jsnack-pari-dispari/js/script.js
+++ jsnack-pari-dispari/js/script.js
@@ -11,21 +11,22 @@ function getRandbyHand() {
     return Math.floor(Math.random()*5 + 1);
 }
 // aziono lo script con le mie funzioni
+// scelta di pari o dispari
 let userChoice;
 do {
     userChoice = prompt("inserisci pari o dispari");
 } while (userChoice != "pari" && userChoice != "dispari")
-
+// scelta del numero
 let userValue;
 do {
     userValue = parseInt(prompt("inserisci un numero da 1 a 5"));
 } while (isNaN(userValue) || userValue < 1 || userValue > 5)
-
+// numero generato per il pc
 pcValue = getRandbyHand();
-
+// somma dei numeri
 const sumValue = userValue + pcValue;
 console.log(sumValue);
-
+// risposta del programma in base ai dai di input utilizzando la funzione isEven creata all'inizio
 if(isEven(sumValue) && userChoice == "pari" || !(isEven(sumValue)) && userChoice == "dispari") {
     console.log("hai vinto");
 } else {
